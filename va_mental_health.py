d91343f57ae84014cfa19504aa7b4ab0c3329560
jbucanay <gilbert.baracka@gmail.com>
2022-02-21 23:08:23 -0700

    csv created

--- va_mental_health.py
+++ va_mental_health.py
@@ -3,20 +3,35 @@
 #if val number with commas -> remove commas from the number
 # csv should have header from the keys in the json file
 
+
 import json 
 import csv
 
 def read_json():
-    fl = open('station-data.json', 'r')
-    data = json.load(fl)
-    print(data[0])
-    print(data[1])
+    with open('station-data.json', 'r') as json_file:
+        data = json.load(json_file)
+        for record in data:
+            if record.get('ValueType') == 'Percent':
+                modify_value = round(float(record.get('Value').replace('%',''))/100, 2)
+                record['Value'] = str(modify_value)
+  
+            if record.get('ValueType') == 'Number':
+                record['Value'] = record['Value'].replace(',', '')
+        return data
+        
 
 def create_csv():
-    pass
+    json_data = read_json()
+    headers = json_data[0].keys()
+    with open('output.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(headers)
+        for record in json_data:
+            rec = [val for val in record.values()]
+            writer.writerow(rec)
 
 def main():
-    read_json()
+    create_csv()
 
 
 
